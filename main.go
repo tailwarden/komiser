9956e8f3f854b535c0c3e8ec859e6ea2ff33f529
LABOUARDY Mohamed <mohamed.labouardy@gmail.com>
2018-02-15 14:58:47 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	. "./models"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -15,35 +16,84 @@ func main() {
 		panic("unable to load SDK config, " + err.Error())
 	}
 
-	for _, region := range getRegions(cfg) {
-		cfg.Region = region
-		ec2Svc := ec2.New(cfg)
-		params := &ec2.DescribeInstancesInput{}
-		req := ec2Svc.DescribeInstancesRequest(params)
-		result, err := req.Send()
-		if err != nil {
-			log.Fatal(err)
-		}
+	fmt.Println(describeInstances(cfg))
+}
 
-		for _, reservation := range result.Reservations {
-			for _, instance := range reservation.Instances {
-				fmt.Println(instance.Tags)
+func describeInstances(cfg aws.Config) InstanceLifecycle {
+	output := InstanceLifecycle{}
+	for _, region := range getRegions(cfg) {
+		instances := getInstances(cfg, region.Name)
+		for _, instance := range instances {
+			switch instance.State {
+			case "pending":
+				output.Pending++
+				break
+			case "rebooting":
+				output.Rebooting++
+				break
+			case "running":
+				output.Running++
+				break
+			case "shutting-down":
+				output.ShuttingDown++
+				break
+			case "terminated":
+				output.Terminated++
+				break
+			case "stopping":
+				output.Stopping++
+				break
+			case "stopped":
+				output.Stopped++
+				break
 			}
 		}
 	}
+	return output
 }
 
-func getRegions(cfg aws.Config) []string {
+func getInstances(cfg aws.Config, region string) []EC2 {
+	cfg.Region = region
+	ec2Svc := ec2.New(cfg)
+	params := &ec2.DescribeInstancesInput{}
+	req := ec2Svc.DescribeInstancesRequest(params)
+	result, err := req.Send()
+	if err != nil {
+		log.Fatal(err)
+	}
+	listOfInstances := make([]EC2, 0)
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			instanceType, _ := instance.InstanceType.MarshalValue()
+			instanceState, _ := instance.State.Name.MarshalValue()
+			instanceTags := make([]string, 0)
+			for _, tag := range instance.Tags {
+				instanceTags = append(instanceTags, *tag.Value)
+			}
+			listOfInstances = append(listOfInstances, EC2{
+				ID:           *instance.InstanceId,
+				InstanceType: instanceType,
+				LaunchTime:   *instance.LaunchTime,
+				Tags:         instanceTags,
+				State:        instanceState,
+			})
+		}
+	}
+	return listOfInstances
+}
+
+func getRegions(cfg aws.Config) []Region {
 	svc := ec2.New(cfg)
 	req := svc.DescribeRegionsRequest(&ec2.DescribeRegionsInput{})
 	regions, err := req.Send()
 	if err != nil {
 		log.Fatal(err)
 	}
-	listOfRegions := make([]string, 0, len(regions.Regions))
+	listOfRegions := make([]Region, 0, len(regions.Regions))
 	for _, region := range regions.Regions {
-		fmt.Println(*region.RegionName)
-		listOfRegions = append(listOfRegions, *region.RegionName)
+		listOfRegions = append(listOfRegions, Region{
+			Name: *region.RegionName,
+		})
 	}
 	return listOfRegions
 }
