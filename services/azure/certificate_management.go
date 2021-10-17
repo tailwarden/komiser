45955d5273c914a008ea2c1455170c39b1dda41b
Annaraya Narasagond <annarayanarasagond@gmail.com>
2021-10-17 08:13:13 +0530

    Remove nil check fot client

--- services/azure/certificate_management.go
+++ services/azure/certificate_management.go
@@ -17,21 +17,18 @@ func (azure Azure) ListCertificates(subscriptionID string) (int64, error) {
 	var filter string
 	var sum int64
 	ctx := context.Background()
-	if certsClient != nil {
-		certificateCollectionPage, err := certsClient.List(ctx, filter)
+	certificateCollectionPage, err := certsClient.List(ctx, filter)
 
-		for err != nil {
-			certCollection := certificateCollectionPage.Response()
-			sum += int64(len(certCollection.Count))
-			err = certificateCollectionPage.NextWithContext(ctx)
-		}
-
-		if err != nil {
-			return sum, err
-		}
-
-		//return sum, nil
+	for err != nil {
+		certCollection := certificateCollectionPage.Response()
+		sum += int64(len(certCollection.Count))
+		err = certificateCollectionPage.NextWithContext(ctx)
 	}
+
+	if err != nil {
+		return sum, err
+	}
+
 	return sum, nil
 }
 
@@ -41,32 +38,29 @@ func (azure Azure) ListExpiredCertificates(subscriptionID string) (int64, error)
 	var sum int64
 	ctx := context.Background()
 
-	if certsClient != nil {
-		certificateCollectionPage, err := certsClient.List(ctx, filter)
+	certificateCollectionPage, err := certsClient.List(ctx, filter)
 
-		for err != nil {
-			certCollection := certificateCollectionPage.Response()
-			certContracts := certCollection.Value
-			var expiredCertCount int64
-			for _, certContract := range certContracts {
-				year, month, day := time.Now().Date()
-				currentDate := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
-				timeLapsed := currentDate.Sub(certContract.ExpirationDate)
-				if timeLapsed.Hours() > 0 {
-					//No action needed as Certificate has not expired
-				} else {
-					expiredCertCount++
-				}
+	for err != nil {
+		certCollection := certificateCollectionPage.Response()
+		certContracts := certCollection.Value
+		var expiredCertCount int64
+		for _, certContract := range certContracts {
+			year, month, day := time.Now().Date()
+			currentDate := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+			timeLapsed := currentDate.Sub(certContract.ExpirationDate)
+			if timeLapsed.Hours() > 0 {
+				//No action needed as Certificate has not expired
+			} else {
+				expiredCertCount++
 			}
-			sum += int64(len(certCollection.Count) - expiredCertCount)
-			err = certificateCollectionPage.NextWithContext(ctx)
 		}
-
-		if err != nil {
-			return sum, err
-		}
-
-		//return sum, nil
+		sum += int64(len(certCollection.Count) - expiredCertCount)
+		err = certificateCollectionPage.NextWithContext(ctx)
 	}
+
+	if err != nil {
+		return sum, err
+	}
+
 	return sum, nil
 }
