dca55e7ed8027eb8467834f99bd3432002ffe34f
Victor F. Santos <victor.gaard@gmail.com>
2022-11-11 15:23:00 +0100

    chore: changed old Komiser logo & favicon for the new one

--- dashboard/utils/formatNumber.ts
+++ dashboard/utils/formatNumber.ts
@@ -1,5 +1,11 @@
-function formatNumber(number: number) {
-  return new Intl.NumberFormat('en-US', {
+function formatNumber(number: number, display?: 'full') {
+  if (display === 'full') {
+    return new Intl.NumberFormat(undefined, {
+      notation: 'standard'
+    }).format(Number(number));
+  }
+
+  return new Intl.NumberFormat(undefined, {
     notation: 'compact',
     compactDisplay: 'short'
   }).format(Number(number));
