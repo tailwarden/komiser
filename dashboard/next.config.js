31ff18a31e27a96b76e4b8fc411210c5108df95b
Mohamed Labouardy <mohamed@labouardy.com>
2023-01-31 14:01:49 +0100

    chore: fix next.js image optimization

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  images: {
+    unoptimized: true
+}
 };
 
 module.exports = nextConfig;
