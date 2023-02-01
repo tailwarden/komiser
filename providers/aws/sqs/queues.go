c63121b7cc83e86977ce516b9d4033516add9d3b
Mohamed Labouardy <mohamed@labouardy.com>
2023-02-01 14:46:22 +0100

    fix: sqs metrics fetching

--- providers/aws/sqs/queues.go
+++ providers/aws/sqs/queues.go
@@ -58,7 +58,7 @@ func Queues(ctx context.Context, client ProviderClient) ([]Resource, error) {
 			}
 
 			nbOfMessagesSent := 0.0
-			if len(metricsNbOfMessagesSentOutput.Datapoints) > 0 {
+			if metricsNbOfMessagesSentOutput != nil && len(metricsNbOfMessagesSentOutput.Datapoints) > 0 {
 				nbOfMessagesSent = *metricsNbOfMessagesSentOutput.Datapoints[0].Sum
 			}
 
@@ -110,7 +110,7 @@ func Queues(ctx context.Context, client ProviderClient) ([]Resource, error) {
 			}
 
 			nbOfMessagesDeleted := 0.0
-			if len(metricsNbOfMessagesDeletedOutput.Datapoints) > 0 {
+			if metricsNbOfMessagesDeletedOutput != nil && len(metricsNbOfMessagesDeletedOutput.Datapoints) > 0 {
 				nbOfMessagesDeleted = *metricsNbOfMessagesDeletedOutput.Datapoints[0].Sum
 			}
 
