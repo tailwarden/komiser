ed5a3b293682730e6312cb1134f0132867ececca
Mahendra Bagul <bagulm123@gmail.com>
2023-02-13 08:52:41 +0530

    feat: add lke-clusters in linode

---
+++ providers/linode/compute/lke_clusters.go
@@ -0,0 +1,64 @@
+package compute
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/linode/linodego"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/tailwarden/komiser/models"
+	. "github.com/tailwarden/komiser/models"
+	"github.com/tailwarden/komiser/providers"
+)
+
+func LKEClusters(ctx context.Context, client providers.ProviderClient) ([]Resource, error) {
+	resources := make([]Resource, 0)
+
+	lkeClusters, err := client.LinodeClient.ListLKEClusters(ctx, &linodego.ListOptions{})
+	if err != nil {
+		return resources, err
+	}
+
+	for _, lkeCluster := range lkeClusters {
+		tags := make([]Tag, 0)
+		for _, tag := range lkeCluster.Tags {
+			if strings.Contains(tag, ":") {
+				parts := strings.Split(tag, ":")
+				tags = append(tags, models.Tag{
+					Key:   parts[0],
+					Value: parts[1],
+				})
+			} else {
+				tags = append(tags, models.Tag{
+					Key:   tag,
+					Value: tag,
+				})
+			}
+		}
+
+		resources = append(resources, models.Resource{
+			Provider:   "Linode",
+			Account:    client.Name,
+			Service:    "LKE",
+			Region:     lkeCluster.Region,
+			ResourceId: fmt.Sprintf("%s", lkeCluster.ID),
+			Cost:       0,
+			Name:       lkeCluster.Label,
+			FetchedAt:  time.Now(),
+			CreatedAt:  *lkeCluster.Created,
+			Tags:       tags,
+			Link:       fmt.Sprintf("https://cloud.linode.com/kubernetes/clusters/%d", lkeCluster.ID),
+		})
+	}
+
+	log.WithFields(log.Fields{
+		"provider":  "Linode",
+		"account":   client.Name,
+		"service":   "LKE",
+		"resources": len(resources),
+	}).Info("Fetched resources")
+	return resources, nil
+}
