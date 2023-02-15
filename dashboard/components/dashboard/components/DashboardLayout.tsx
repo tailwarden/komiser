4be911d3209b244382c5545f869dd45aad6f4656
Victor F. Santos <victor.gaard@gmail.com>
2023-02-15 12:38:44 +0100

    style: change chart and tooltip colors to match design

--- dashboard/components/dashboard/components/DashboardLayout.tsx
+++ dashboard/components/dashboard/components/DashboardLayout.tsx
@@ -6,7 +6,7 @@ type DashboardLayoutProps = {
 
 function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className="relative flex flex-col gap-6">
+    <div className="flex flex-col gap-6">
       <p className="flex items-center gap-2 text-lg font-medium text-black-900">
         Dashboard overview
       </p>
--- dashboard/components/dashboard/components/cloud-map/DashboardCloudMapChart.tsx
+++ dashboard/components/dashboard/components/cloud-map/DashboardCloudMapChart.tsx
@@ -23,66 +23,65 @@ function DashboardCloudMapChart({
     regions && regions.sort((a, b) => a.resources - b.resources);
 
   return (
-    <div className="relative">
-      <ComposableMap
-        projection="geoNaturalEarth1"
-        height={482}
-        width={820}
-        projectionConfig={{
-          center: [14, 0]
-        }}
-      >
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => (
-              <Geography
-                tabIndex={-1}
-                key={geo.rsmKey}
-                geography={geo}
-                fill={'#DAD3E2'}
-                stroke={'white'}
-                style={{
-                  default: { outline: 'none' },
-                  hover: { outline: 'none' },
-                  pressed: { outline: 'none' }
-                }}
-              />
-            ))
-          }
-        </Geographies>
-        {regionsAscendingByNumberOfResources &&
-          regionsAscendingByNumberOfResources.map((region, idx) => (
-            <Marker
-              key={idx}
-              coordinates={[Number(region.longitude), Number(region.latitude)]}
-              onMouseEnter={e =>
-                setTooltip({
-                  name: region.name,
-                  label: region.label,
-                  resources: region.resources,
-                  x: e.pageX,
-                  y: e.pageY
-                })
-              }
-              onMouseLeave={() => setTooltip(undefined)}
-            >
-              {region.resources > 0 && (
-                <circle
-                  r={10}
-                  fill="#56BA5B"
-                  className="pointer-events-none animate-wide-pulse"
-                />
-              )}
+    <ComposableMap
+      projection="geoNaturalEarth1"
+      height={482}
+      width={820}
+      projectionConfig={{
+        center: [14, 0]
+      }}
+    >
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map(geo => (
+            <Geography
+              tabIndex={-1}
+              key={geo.rsmKey}
+              geography={geo}
+              fill="#F4F9F9"
+              stroke="#95A3A3"
+              strokeWidth={0.35}
+              style={{
+                default: { outline: 'none' },
+                hover: { outline: 'none' },
+                pressed: { outline: 'none' }
+              }}
+            />
+          ))
+        }
+      </Geographies>
+      {regionsAscendingByNumberOfResources &&
+        regionsAscendingByNumberOfResources.map((region, idx) => (
+          <Marker
+            key={idx}
+            coordinates={[Number(region.longitude), Number(region.latitude)]}
+            onMouseEnter={e =>
+              setTooltip({
+                name: region.name,
+                label: region.label,
+                resources: region.resources,
+                x: e.pageX,
+                y: e.pageY
+              })
+            }
+            onMouseLeave={() => setTooltip(undefined)}
+          >
+            {region.resources > 0 && (
               <circle
-                r={10}
-                fill={region.resources === 0 ? '#978EA1' : '#56BA5B'}
-                stroke={'white'}
-                strokeWidth={1.5}
+                r={9}
+                fill="#387BEB"
+                className="pointer-events-none animate-wide-pulse"
               />
-            </Marker>
-          ))}
-      </ComposableMap>
-    </div>
+            )}
+            <circle
+              r={9}
+              fill={region.resources === 0 ? '#95A3A3' : '#387BEB'}
+              stroke="white"
+              strokeWidth={1.5}
+            />
+          </Marker>
+        ))}
+    </ComposableMap>
   );
 }
 
