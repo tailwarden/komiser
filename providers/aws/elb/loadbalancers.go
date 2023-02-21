839745f40be7bbc24b4984a604439480a87aa787
Fotis Papadamis <fpapadamis@convertgroup.com>
2023-02-21 18:29:58 +0200

    Refactored ELB provider, implemented elasticloadbalancingv2 service

--- providers/aws/elb/loadbalancers.go
+++ providers/aws/elb/loadbalancers.go
@@ -7,9 +7,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancing"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
 	. "github.com/tailwarden/komiser/models"
 	. "github.com/tailwarden/komiser/providers"
 )
@@ -20,71 +18,57 @@ func BeginningOfMonth(date time.Time) time.Time {
 
 func LoadBalancers(ctx context.Context, client ProviderClient) ([]Resource, error) {
 	resources := make([]Resource, 0)
-	var config elasticloadbalancing.DescribeLoadBalancersInput
-	elbClient := elasticloadbalancing.NewFromConfig(*client.AWSClient)
+	var config elasticloadbalancingv2.DescribeLoadBalancersInput
+	elbClient := elasticloadbalancingv2.NewFromConfig(*client.AWSClient)
 
-	stsClient := sts.NewFromConfig(*client.AWSClient)
-	stsOutput, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
+	output, err := elbClient.DescribeLoadBalancers(ctx, &config)
 	if err != nil {
 		return resources, err
 	}
 
-	accountId := stsOutput.Account
-
-	for {
-		output, err := elbClient.DescribeLoadBalancers(ctx, &config)
+	for _, loadbalancer := range output.LoadBalancers {
+		resourceArn := *loadbalancer.LoadBalancerArn
+		outputTags, err := elbClient.DescribeTags(ctx, &elasticloadbalancingv2.DescribeTagsInput{
+			ResourceArns: []string{resourceArn},
+		})
 		if err != nil {
 			return resources, err
 		}
 
-		for _, loadbalancer := range output.LoadBalancerDescriptions {
-			resourceArn := fmt.Sprintf("arn:aws:elasticloadbalancing:%s:%s:loadbalancer/app/%s", client.AWSClient.Region, *accountId, *loadbalancer.LoadBalancerName)
-			outputTags, err := elbClient.DescribeTags(ctx, &elasticloadbalancing.DescribeTagsInput{
-				LoadBalancerNames: config.LoadBalancerNames,
-			})
-
-			tags := make([]Tag, 0)
-			if err == nil {
-				for _, tagDescription := range outputTags.TagDescriptions {
-					for _, tag := range tagDescription.Tags {
-						tags = append(tags, Tag{
-							Key:   *tag.Key,
-							Value: *tag.Value,
-						})
-					}
-				}
+		tags := make([]Tag, 0)
+		for _, tagDescription := range outputTags.TagDescriptions {
+			for _, tag := range tagDescription.Tags {
+				tags = append(tags, Tag{
+					Key:   *tag.Key,
+					Value: *tag.Value,
+				})
 			}
-
-			startOfMonth := BeginningOfMonth(time.Now())
-			hourlyUsage := 0
-			if (*loadbalancer.CreatedTime).Before(startOfMonth) {
-				hourlyUsage = int(time.Now().Sub(startOfMonth).Hours())
-			} else {
-				hourlyUsage = int(time.Now().Sub(*loadbalancer.CreatedTime).Hours())
-			}
-			monthlyCost := float64(hourlyUsage) * 0.0225
-
-			resources = append(resources, Resource{
-				Provider:   "AWS",
-				Account:    client.Name,
-				Service:    "ELB",
-				ResourceId: resourceArn,
-				Region:     client.AWSClient.Region,
-				Name:       *loadbalancer.LoadBalancerName,
-				Cost:       monthlyCost,
-				Tags:       tags,
-				CreatedAt:  *loadbalancer.CreatedTime,
-				FetchedAt:  time.Now(),
-				Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/ec2/home?region=%s#/LoadBalancer:loadBalancerArn=%s", client.AWSClient.Region, client.AWSClient.Region, resourceArn),
-			})
 		}
 
-		if aws.ToString(output.NextMarker) == "" {
-			break
+		startOfMonth := BeginningOfMonth(time.Now())
+		hourlyUsage := 0
+		if (*loadbalancer.CreatedTime).Before(startOfMonth) {
+			hourlyUsage = int(time.Since(startOfMonth).Hours())
+		} else {
+			hourlyUsage = int(time.Since(*loadbalancer.CreatedTime).Hours())
 		}
+		monthlyCost := float64(hourlyUsage) * 0.0225
 
-		config.Marker = output.NextMarker
+		resources = append(resources, Resource{
+			Provider:   "AWS",
+			Account:    client.Name,
+			Service:    "ELB",
+			ResourceId: resourceArn,
+			Region:     client.AWSClient.Region,
+			Name:       *loadbalancer.LoadBalancerName,
+			Cost:       monthlyCost,
+			Tags:       tags,
+			CreatedAt:  *loadbalancer.CreatedTime,
+			FetchedAt:  time.Now(),
+			Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/ec2/home?region=%s#/LoadBalancer:loadBalancerArn=%s", client.AWSClient.Region, client.AWSClient.Region, resourceArn),
+		})
 	}
+
 	log.WithFields(log.Fields{
 		"provider":  "AWS",
 		"account":   client.Name,
@@ -92,5 +76,6 @@ func LoadBalancers(ctx context.Context, client ProviderClient) ([]Resource, erro
 		"service":   "ELB",
 		"resources": len(resources),
 	}).Info("Fetched resources")
+
 	return resources, nil
 }
