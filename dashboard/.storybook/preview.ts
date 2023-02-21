d6634207cf837ad86197601447c1074c7e9b29ae
Victor F. Santos <victor.gaard@gmail.com>
2023-02-21 12:02:45 +0100

    chore: install storybook

---
+++ dashboard/.storybook/preview.ts
@@ -0,0 +1,12 @@
+export const parameters = {
+  backgrounds: {
+    default: 'light'
+  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+};
