23a74fd44cf43106f55d459a85bf70b488b13bbc
Victor F. Santos <victor.gaard@gmail.com>
2023-02-22 15:19:21 +0100

    feat: add empty state to storybook

---
+++ dashboard/components/empty-state/EmptyState.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import EmptyState from './EmptyState';
+import mockEmptyStateProps from './EmptyState.mocks';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta: Meta<typeof EmptyState> = {
+  title: 'Komiser/Empty State',
+  component: EmptyState,
+  tags: ['autodocs'],
+  argTypes: {
+    action: {
+      control: 'hide'
+    }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof EmptyState>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    ...mockEmptyStateProps.base
+  }
+};
