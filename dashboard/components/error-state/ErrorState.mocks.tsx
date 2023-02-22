d06412d6b99ba0d87785b0802aa29d2bd9984452
Victor F. Santos <victor.gaard@gmail.com>
2023-02-22 15:15:56 +0100

    feat: add error state to storybook

---
+++ dashboard/components/error-state/ErrorState.mocks.tsx
@@ -0,0 +1,19 @@
+import Button from '../button/Button';
+import { ErrorStateProps } from './ErrorState';
+
+const base: ErrorStateProps = {
+  title: 'Network request error',
+  message:
+    'There was an error fetching the cloud accounts. Please refer to the logs for more info and try again.',
+  action: (
+    <Button style="outline" size="lg">
+      Refresh the page
+    </Button>
+  )
+};
+
+const mockErrorStateProps = {
+  base
+};
+
+export default mockErrorStateProps;
