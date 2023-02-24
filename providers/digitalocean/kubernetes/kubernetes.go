fc49fc7557e9eabb50a7793fc184b37e0dddace3
Mohamed Labouardy <mohamed@labouardy.com>
2023-02-24 15:50:28 +0100

    fix: conflicts

--- providers/digitalocean/kubernetes/kubernetes.go
+++ providers/digitalocean/kubernetes/kubernetes.go
@@ -1,4 +1,4 @@
-package k8s
+package kubernetes
 
 import (
 	"context"
@@ -41,7 +41,7 @@ func Clusters(ctx context.Context, client providers.ProviderClient) ([]models.Re
 			Provider:   "DigitalOcean",
 			Account:    client.Name,
 			Service:    "Kubernetes",
-			ResourceId: fmt.Sprintf("%s", kubernetesCluster.ID),
+			ResourceId: fmt.Sprintf("%d", kubernetesCluster.ID),
 			Region:     kubernetesCluster.RegionSlug,
 			Name:       kubernetesCluster.Name,
 			Tags:       tags,
