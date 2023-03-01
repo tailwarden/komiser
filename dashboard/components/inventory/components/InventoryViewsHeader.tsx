ed49d6e6007e70a4b35867b708b41637e3957c2f
Victor F. Santos <victor.gaard@gmail.com>
2023-03-01 16:17:49 +0100

    feat: add alert button on view page

--- dashboard/components/inventory/components/InventoryViewsHeader.tsx
+++ dashboard/components/inventory/components/InventoryViewsHeader.tsx
@@ -65,31 +65,29 @@ function InventoryViewsHeader({
 
   return (
     <div className="relative">
-      <div className="flex items-center gap-2 text-lg font-medium text-black-900">
-        {currentView && (
-          <>
-            <span>{currentView.name}</span>
-            <Button style="ghost" size="xs" onClick={openDropdown}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeMiterlimit="10"
-                  strokeWidth="1.5"
-                  d="M19.92 8.95l-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
-                ></path>
-              </svg>
-            </Button>
-          </>
-        )}
-      </div>
+      {currentView && (
+        <div className="flex items-center gap-2 text-lg font-medium text-black-900">
+          <span>{currentView.name}</span>
+          <Button style="ghost" size="xs" onClick={openDropdown}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+                strokeWidth="1.5"
+                d="M19.92 8.95l-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
+              ></path>
+            </svg>
+          </Button>
+        </div>
+      )}
 
       {dropdownIsOpen && (
         <>
