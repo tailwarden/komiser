336bbd1230938de1d98002b30a39e682e9849a4b
Mariano Benítez Mulet <mariano.benitez@edreamsodigeo.com>
2023-03-10 12:49:52 +0100

    Housecleaning after approval

--- providers/aws/ec2/autoscaling_groups.go
+++ providers/aws/ec2/autoscaling_groups.go
@@ -13,7 +13,8 @@ import (
 	. "github.com/tailwarden/komiser/providers"
 )
 
-// This is to comply with current architecture as described in CONTRIBUTING.md
+const AWS_SERVICE_NAME_ASG = "AutoScalingGroup"
+
 func AutoScalingGroups(ctx context.Context, clientProvider ProviderClient) ([]Resource, error) {
 	client := autoscaling.NewFromConfig(*clientProvider.AWSClient)
 
@@ -27,8 +28,6 @@ func AutoScalingGroups(ctx context.Context, clientProvider ProviderClient) ([]Re
 	return d.Discover()
 }
 
-// As I see it, this could be a struct that implements the Discoverer interface
-// This would allow us to test it in isolation
 type ASGDiscoverer struct {
 	Client      AutoScalingGroupClient
 	Ctx         context.Context
@@ -36,13 +35,6 @@ type ASGDiscoverer struct {
 	Region      string
 }
 
-// This could possibly be the only method the interface requires
-// A good job has been done at making []Resource standard across providers and services
-// Maybe we could add a factory method to the Resource struct to make it easier to create
-
-// I am not thrilled about the way I deal with the account name and region.
-// I would rather see if there's a way to fetch them from the client itself.
-// Else, I would create a simple struct containing those two values and pass it around
 func (d ASGDiscoverer) Discover() ([]Resource, error) {
 
 	resources := make([]Resource, 0)
@@ -66,7 +58,7 @@ func (d ASGDiscoverer) Discover() ([]Resource, error) {
 			resources = append(resources, Resource{
 				Provider:   "AWS",
 				Account:    d.AccountName,
-				Service:    "AutoScalingGroup",
+				Service:    AWS_SERVICE_NAME_ASG,
 				Region:     d.Region,
 				ResourceId: *asg.AutoScalingGroupARN,
 				Cost:       0,
