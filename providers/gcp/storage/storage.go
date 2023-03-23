20fbcde1b6300295ff5db0f2f85d6eeefa75e7d2
Enes Kaya <eneskaya@me.com>
2023-03-23 17:22:32 +0100

    feat: calculate bucket size correctly

--- providers/gcp/storage/storage.go
+++ providers/gcp/storage/storage.go
@@ -19,6 +19,7 @@ import (
 	"google.golang.org/api/option"
 )
 
+// GetBucketSize returns the size of a bucket in bytes
 func GetBucketSize(ctx context.Context, client providers.ProviderClient, bucketName string) (int64, error) {
 	monitoringClient, err := monitoring.NewMetricClient(ctx, option.WithCredentials(client.GCPClient.Credentials))
 	if err != nil {
@@ -26,16 +27,18 @@ func GetBucketSize(ctx context.Context, client providers.ProviderClient, bucketN
 		return 0, err
 	}
 
+	beginningOfMonth := time.Date(time.Now().Year(), time.Now().Month(), 1, 0, 0, 0, 0, time.UTC).Unix()
+
 	req := &monitoringpb.ListTimeSeriesRequest{
 		Name:   "projects/" + client.GCPClient.Credentials.ProjectID,
 		Filter: "metric.type=\"storage.googleapis.com/storage/total_bytes\" resource.type=\"gcs_bucket\" resource.label.bucket_name=\"" + bucketName + "\"",
 		Interval: &monitoringpb.TimeInterval{
+			StartTime: &timestamp.Timestamp{Seconds: beginningOfMonth},
 			EndTime:   &timestamp.Timestamp{Seconds: time.Now().Unix()},
-			StartTime: &timestamp.Timestamp{Seconds: time.Now().Add(-1 * time.Hour).Unix()},
 		},
 		Aggregation: &monitoringpb.Aggregation{
 			AlignmentPeriod:  &duration.Duration{Seconds: 60},
-			PerSeriesAligner: monitoringpb.Aggregation_ALIGN_MEAN,
+			PerSeriesAligner: monitoringpb.Aggregation_ALIGN_SUM,
 		},
 	}
 
@@ -92,11 +95,14 @@ func Buckets(ctx context.Context, client providers.ProviderClient) ([]models.Res
 
 		bucketSize, err := GetBucketSize(ctx, client, bucket.Name)
 		if err != nil {
-			log.WithError(err).Errorf("failed to get bucket size")
+			log.WithError(err).WithFields(log.Fields{
+				"bucket": bucket.Name,
+			}).Debug("failed to get bucket size, skipping cost calculation")
 		}
 
 		if bucketSize > 0 {
-			monthlyCost = float64(bucketSize) * 0.20
+			bucketSizeInGB := float64(bucketSize) / 1024 / 1024 / 1024
+			monthlyCost = float64(bucketSizeInGB) * 0.20
 		}
 
 		resources = append(resources, models.Resource{
