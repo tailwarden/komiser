4eeb0cba88f022f844a222b2feefbfb068ff853f
ShubhamPalriwala <spalriwalau@gmail.com>
2023-04-17 19:28:23 +0530

    feat: only query user created policies in the root

--- providers/aws/iam/policies.go
+++ providers/aws/iam/policies.go
@@ -9,13 +9,16 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
 	. "github.com/tailwarden/komiser/models"
 	. "github.com/tailwarden/komiser/providers"
 )
 
 func Policies(ctx context.Context, client ProviderClient) ([]Resource, error) {
 	resources := make([]Resource, 0)
-	var configPolicies iam.ListPoliciesInput
+	configPolicies := iam.ListPoliciesInput{
+		Scope: types.PolicyScopeTypeLocal,
+	}
 	var configTags iam.ListPolicyTagsInput
 	iamClient := iam.NewFromConfig(*client.AWSClient)
 
@@ -26,44 +29,41 @@ func Policies(ctx context.Context, client ProviderClient) ([]Resource, error) {
 		}
 
 		for _, policy := range outputPolicies.Policies {
-			// only fetch user created policies
-			if aws.ToString(policy.Arn)[:20] != "arn:aws:iam::aws:pol" {
-				tags := make([]Tag, 0)
-				for {
-					configTags.PolicyArn = policy.Arn
-					outputPolicyTags, err := iamClient.ListPolicyTags(ctx, &configTags)
-					if err != nil {
-						return resources, err
-					}
-
-					for _, t := range outputPolicyTags.Tags {
-						tags = append(tags, Tag{
-							Key:   *t.Key,
-							Value: *t.Value,
-						})
-					}
-
-					if aws.ToString(outputPolicyTags.Marker) == "" {
-						break
-					}
-
-					configTags.Marker = outputPolicyTags.Marker
+			tags := make([]Tag, 0)
+			for {
+				configTags.PolicyArn = policy.Arn
+				outputPolicyTags, err := iamClient.ListPolicyTags(ctx, &configTags)
+				if err != nil {
+					return resources, err
 				}
 
-				resources = append(resources, Resource{
-					Provider:   "AWS",
-					Account:    client.Name,
-					Service:    "IAM Policy",
-					ResourceId: *policy.Arn,
-					Region:     client.AWSClient.Region,
-					Name:       *policy.PolicyName,
-					Cost:       0,
-					CreatedAt:  *policy.CreateDate,
-					Tags:       tags,
-					FetchedAt:  time.Now(),
-					Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/iam/home#/policies/%s", client.AWSClient.Region, *policy.Arn),
-				})
+				for _, t := range outputPolicyTags.Tags {
+					tags = append(tags, Tag{
+						Key:   *t.Key,
+						Value: *t.Value,
+					})
+				}
+
+				if aws.ToString(outputPolicyTags.Marker) == "" {
+					break
+				}
+
+				configTags.Marker = outputPolicyTags.Marker
 			}
+
+			resources = append(resources, Resource{
+				Provider:   "AWS",
+				Account:    client.Name,
+				Service:    "IAM Policy",
+				ResourceId: *policy.Arn,
+				Region:     client.AWSClient.Region,
+				Name:       *policy.PolicyName,
+				Cost:       0,
+				CreatedAt:  *policy.CreateDate,
+				Tags:       tags,
+				FetchedAt:  time.Now(),
+				Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/iam/home#/policies/%s", client.AWSClient.Region, *policy.Arn),
+			})
 		}
 
 		if aws.ToString(outputPolicies.Marker) == "" {
