4f24106c926d3ac70443b39d1024f6d7399d6610
ShubhamPalriwala <spalriwalau@gmail.com>
2023-04-19 23:29:50 +0530

    feat: support AWS Service Catalog Applications

---
+++ providers/aws/servicecatalog/applications.go
@@ -0,0 +1,58 @@
+package servicecatalog
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/servicecatalog"
+	"github.com/tailwarden/komiser/models"
+	"github.com/tailwarden/komiser/providers"
+)
+
+func Products(ctx context.Context, client providers.ProviderClient) ([]models.Resource, error) {
+	resources := make([]models.Resource, 0)
+
+	serviceCatalogsClient := servicecatalog.NewFromConfig(*client.AWSClient)
+
+	input := &servicecatalog.SearchProductsAsAdminInput{}
+
+	for {
+		output, err := serviceCatalogsClient.SearchProductsAsAdmin(ctx, input)
+		if err != nil {
+			return resources, err
+		}
+
+		for _, product := range output.ProductViewDetails {
+			resources = append(resources, models.Resource{
+				Provider:   "AWS",
+				Account:    client.Name,
+				Service:    "Service Catalog Application",
+				ResourceId: aws.ToString(product.ProductViewSummary.ProductId),
+				Region:     client.AWSClient.Region,
+				Name:       aws.ToString(product.ProductViewSummary.Name),
+				Cost:       0,
+				FetchedAt:  time.Now(),
+				Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/servicecatalog/home?region=%s#/admin-products/%s", client.AWSClient.Region, client.AWSClient.Region, aws.ToString(product.ProductViewSummary.ProductId)),
+			})
+		}
+
+		if output.NextPageToken == nil {
+			break
+		}
+
+		input.PageToken = output.NextPageToken
+	}
+	log.WithFields(log.Fields{
+		"provider":  "AWS",
+		"account":   client.Name,
+		"region":    client.AWSClient.Region,
+		"service":   "Service Catalog",
+		"resources": len(resources),
+	}).Info("Fetched resources")
+
+	return resources, nil
+}
