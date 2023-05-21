7d466d2df8243676fbf54f217645c1f2773c542f
Siddarth Kumar <siddarthkay@gmail.com>
2023-05-22 00:23:46 +0530

    feat(rds): add error handling and fetch tags

--- providers/aws/rds/db_proxy_endpoints.go
+++ providers/aws/rds/db_proxy_endpoints.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"time"
-
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 	log "github.com/sirupsen/logrus"
 	"github.com/tailwarden/komiser/models"
 	"github.com/tailwarden/komiser/providers"
+	"time"
 )
 
 func ProxyEndpoints(ctx context.Context, client providers.ProviderClient) ([]models.Resource, error) {
@@ -22,13 +21,42 @@ func ProxyEndpoints(ctx context.Context, client providers.ProviderClient) ([]mod
 	for {
 		output, err := rdsClient.DescribeDBProxyEndpoints(ctx, &config)
 		if err != nil {
-			return resources, err
+			return resources, fmt.Errorf("error describing DB proxy endpoints: %w", err)
 		}
 
 		for _, endpoint := range output.DBProxyEndpoints {
-			tags := make([]models.Tag, 0)
-			//TODO (siddarthkay) : double check on tag retrieval
+
+			if endpoint.DBProxyEndpointName == nil {
+				log.Warn("DBProxyEndpointName is nil")
+				continue
+			}
+
+			if endpoint.DBProxyEndpointArn == nil {
+				log.Warn("DBProxyEndpointArn is nil")
+				continue
+			}
+
 			_endpointName := *endpoint.DBProxyEndpointName
+
+			// Fetch tags
+			tagConfig := rds.ListTagsForResourceInput{
+				ResourceName: endpoint.DBProxyEndpointArn,
+			}
+
+			tagOutput, err := rdsClient.ListTagsForResource(ctx, &tagConfig)
+			if err != nil {
+				return resources, fmt.Errorf("error listing tags for resource %s: %w", *endpoint.DBProxyEndpointArn, err)
+			}
+
+			tags := make([]models.Tag, 0)
+			for _, tag := range tagOutput.TagList {
+				if tag.Key != nil && tag.Value != nil {
+					tags = append(tags, models.Tag{
+						Key:   *tag.Key,
+						Value: *tag.Value,
+					})
+				}
+			}
 
 			resources = append(resources, models.Resource{
 				Provider:   "AWS",
@@ -39,7 +67,7 @@ func ProxyEndpoints(ctx context.Context, client providers.ProviderClient) ([]mod
 				Name:       _endpointName,
 				FetchedAt:  time.Now(),
 				Tags:       tags,
-				Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/rds/home?region=%s#dbproxy:endpoint=%s", client.AWSClient.Region, client.AWSClient.Region, *endpoint.DBProxyEndpointName),
+				Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/rds/home?region=%s#db-proxy-details:id=%s", client.AWSClient.Region, client.AWSClient.Region, *endpoint.DBProxyEndpointName),
 			})
 		}
 
