d39a7c62b6a4d0d3c3b2724abd819ba2f8d1ccab
aimanafzal <96443146+aimanafzal1@users.noreply.github.com>
2023-07-09 15:43:42 +0500

    Refactored TaskDefinitions

--- providers/aws/ecs/taskDefinitions.go
+++ providers/aws/ecs/taskDefinitions.go
@@ -3,26 +3,31 @@ package ecs
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
 	log "github.com/sirupsen/logrus"
 	. "github.com/tailwarden/komiser/models"
 	. "github.com/tailwarden/komiser/providers"
 )
 
+func extractNameAndRevisionFromArn(input string) []string {
+	var nameAndRevision [2]string
+
+	parts := strings.Split(input, ":")
+	if len(parts) >= 6 {
+		nameAndRevision[0] = strings.Split(parts[5], "/")[1]
+		nameAndRevision[1] = parts[6]
+	}
+	return nameAndRevision[:]
+}
+
 func TaskDefinitions(ctx context.Context, client ProviderClient) ([]Resource, error) {
 	resources := make([]Resource, 0)
 	var config ecs.ListTaskDefinitionsInput
 	ecsClient := ecs.NewFromConfig(*client.AWSClient)
-	stsClient := sts.NewFromConfig(*client.AWSClient)
-	stsoutput, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
-	if err != nil {
-		return resources, err
-	}
-	accountId := stsoutput.Account
 
 	for {
 		output, err := ecsClient.ListTaskDefinitions(context.Background(), &config)
@@ -30,19 +35,20 @@ func TaskDefinitions(ctx context.Context, client ProviderClient) ([]Resource, er
 			return resources, err
 		}
 		for _, taskdefinition := range output.TaskDefinitionArns {
-			resourceArn := fmt.Sprintf("arn:aws:ecs:%s:%s:taskdefinition/%s", client.AWSClient.Region, *accountId, taskdefinition)
+			ecsNameAndRevisition := extractNameAndRevisionFromArn(taskdefinition)
 			resources = append(resources, Resource{
 				Provider:   "AWS",
 				Account:    client.Name,
 				Service:    "ECS Task Definition",
-				ResourceId: resourceArn,
+				ResourceId: taskdefinition,
 				Region:     client.AWSClient.Region,
-				Name:       taskdefinition,
+				Name:       ecsNameAndRevisition[0],
 				Cost:       0,
 				FetchedAt:  time.Now(),
-				Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/ecs/home?#/taskdefinition/%s", client.AWSClient.Region, taskdefinition),
+				Link:       fmt.Sprintf("https://%s.console.aws.amazon.com/ecs/v2/taskdefinitions?%s", client.AWSClient.Region, taskdefinition),
 			})
 		}
+
 		if aws.ToString(output.NextToken) == "" {
 			break
 		}
