d04397df23618a9a263efa62e87d2431910cb181
Abhay Kumar Gupta <abhayetwup123@gmail.com>
2023-07-23 17:30:53 +0530

    Update lke_pools.go

--- providers/linode/linode-lke/lke_pools.go
+++ providers/linode/linode-lke/lke_pools.go
@@ -1,4 +1,4 @@
-package linodelke
+package compute
 
 import (
 	"context"
@@ -17,41 +17,46 @@ type LinodeLKENodePool struct {
 	NodePool *linodego.LKECluster `json:"node_pool"`
 }
 
-func LKENodePools(ctx context.Context, client providers.ProviderClient, linodeLKENodePools []LinodeLKENodePool) ([]Resource, error) {
-	resources := make([]Resource, 0)
+func LKENodePools(ctx context.Context, client providers.ProviderClient) ([]models.Resource, error) {
+	resources := make([]models.Resource, 0)
 
-	for _, lkeNodePool := range linodeLKENodePools {
-		nodePool := lkeNodePool.NodePool
+	lkeClusters, err := client.LinodeClient.ListLKEClusters(ctx, &linodego.ListOptions{})
+	if err != nil {
+		return resources, err
+	}
 
-		tags := make([]Tag, 0)
-		// If Linode LKE node pools have tags, you can handle them here.
-		// Replace the example tags below with your actual tag handling logic.
-		if len(nodePool.Tags) > 0 {
-			tags = append(tags, models.Tag{
-				Key:   "example-key",
-				Value: nodePool.Tags[0],
-			})
+	for _, lkeCluster := range lkeClusters {
+		nodePools, err := client.LinodeClient.ListLKEClusterNodePools(ctx, lkeCluster.ID, &linodego.ListOptions{})
+		if err != nil {
+			return resources, err
 		}
 
-		// Get the count of nodes in the pool.
-		// Note: Linode does not provide direct access to the node count in the node pool.
-		// You may need to handle this based on the context in which you manage the LKE node pools.
-		nodeCount := 0
+		for _, nodePool := range nodePools {
+			tags := make([]models.Tag, 0)
+			// If Linode LKE node pools have tags, you can handle them here.
+			// Replace the example tags below with your actual tag handling logic.
+			if len(nodePool.Tags) > 0 {
+				tags = append(tags, models.Tag{
+					Key:   "example-key",
+					Value: nodePool.Tags[0],
+				})
+			}
 
-		resources = append(resources, models.Resource{
-			Provider:   "Linode",
-			Account:    client.Name,
-			Service:    "Linode Kubernetes Engine",
-			Region:     nodePool.Region,
-			ResourceId: fmt.Sprintf("%d", nodePool.ID),
-			Cost:       0,
-			Name:       nodePool.Label,
-			FetchedAt:  time.Now(),
-			CreatedAt:  time.Time{}, // Update this with the actual created time.
-			Tags:       tags,
-			Link:       fmt.Sprintf("https://cloud.linode.com/kubernetes/clusters/%d", nodePool.ID),
-			// Add any additional fields or data you want to collect here.
-		})
+			resources = append(resources, models.Resource{
+				Provider:   "Linode",
+				Account:    client.Name,
+				Service:    "Linode Kubernetes Engine",
+				Region:     nodePool.Region,
+				ResourceId: fmt.Sprintf("%d", nodePool.ID),
+				Cost:       0,
+				Name:       nodePool.Label,
+				FetchedAt:  time.Now(),
+				CreatedAt:  time.Time{}, // Update this with the actual created time.
+				Tags:       tags,
+				Link:       fmt.Sprintf("https://cloud.linode.com/kubernetes/clusters/%d", lkeCluster.ID),
+				// Add any additional fields or data you want to collect here.
+			})
+		}
 	}
 
 	log.WithFields(log.Fields{
