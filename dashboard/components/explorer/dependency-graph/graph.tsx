5b2af61064325018d4aa74096b61e94ba40ef61f
Alexander Rösel <320272+Traxmaxx@users.noreply.github.com>
2023-09-20 20:12:11 +0200

    fix: improve styling and add click listwnwe

--- dashboard/components/explorer/dependency-graph/graph.tsx
+++ dashboard/components/explorer/dependency-graph/graph.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useCallback, useLayoutEffect } from 'react';
+import React, { useState, memo } from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
-import Cytoscape from 'cytoscape';
+import Cytoscape, { EventObject } from 'cytoscape';
 // @ts-ignore
 import COSEBilkent from 'cytoscape-cose-bilkent';
 
@@ -11,10 +11,20 @@ export type LayoutFlowProps = {
   data: ReactFlowData;
 };
 
+const layout = { name: 'cose-bilkent', animate: true };
+
 Cytoscape.use(COSEBilkent);
 const LayoutFlow = ({ data }: LayoutFlowProps) => {
-  const layout = { name: 'cose-bilkent', animate: true };
-  let cy;
+  const [initDone, setInitDone] = useState(false);
+
+  const cyActionHandlers = (cy: Cytoscape.Core) => {
+    if (!initDone) {
+      cy.on('click', 'node', (evt: EventObject) => {
+        console.info(`Clicked the node with ID ${evt.target.id()}`);
+      });
+      setInitDone(true);
+    }
+  };
 
   return (
     <div className="relative h-full flex-1">
@@ -25,6 +35,8 @@ const LayoutFlow = ({ data }: LayoutFlowProps) => {
             nodes: data.nodes,
             edges: data.edges
           })}
+          maxZoom={4}
+          minZoom={0.25}
           layout={layout}
           stylesheet={[
             {
@@ -32,11 +44,17 @@ const LayoutFlow = ({ data }: LayoutFlowProps) => {
               style: {
                 width: 40,
                 height: 40,
+                shape: 'ellipse',
                 content: 'data(label)',
+                'background-color': 'white',
                 'background-image': node =>
                   node.data('resource') === 'AWS'
                     ? '/assets/img/dependency-graph/aws-node.svg'
-                    : ''
+                    : '',
+                'background-height': 20,
+                'border-color': '#EDEBEE',
+                'border-width': 1,
+                'border-style': 'solid'
               }
             },
             {
@@ -48,10 +66,11 @@ const LayoutFlow = ({ data }: LayoutFlowProps) => {
               }
             }
           ]}
+          cy={(cy: Cytoscape.Core) => cyActionHandlers(cy)}
         />
       </div>
     </div>
   );
 };
 
-export default LayoutFlow;
+export default memo(LayoutFlow);
