aa1ad26dfffcddc89afea43c6b41c1ca8f504260
Azanul <azanulhaque@gmail.com>
2023-09-25 22:52:50 +0530

    k8s pod cost calculation
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- providers/k8s/core/pods.go
+++ providers/k8s/core/pods.go
@@ -6,15 +6,22 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	. "github.com/tailwarden/komiser/models"
+	"github.com/tailwarden/komiser/models"
 	"github.com/tailwarden/komiser/providers"
+	oc "github.com/tailwarden/komiser/providers/k8s/opencost"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func Pods(ctx context.Context, client providers.ProviderClient) ([]Resource, error) {
-	resources := make([]Resource, 0)
+func Pods(ctx context.Context, client providers.ProviderClient) ([]models.Resource, error) {
+	resources := make([]models.Resource, 0)
 
 	var config metav1.ListOptions
+
+	podsCost, err := oc.GetOpencostInfo(client.K8sClient.OpencostBaseUrl, "pod")
+	if err != nil {
+		log.Printf("ERROR: Couldn't get pods info from OpenCost: %v", err)
+		return resources, err
+	}
 
 	for {
 		res, err := client.K8sClient.Client.CoreV1().Pods("").List(ctx, config)
@@ -23,23 +30,23 @@ func Pods(ctx context.Context, client providers.ProviderClient) ([]Resource, err
 		}
 
 		for _, pod := range res.Items {
-			tags := make([]Tag, 0)
+			tags := make([]models.Tag, 0)
 
 			for key, value := range pod.Labels {
-				tags = append(tags, Tag{
+				tags = append(tags, models.Tag{
 					Key:   key,
 					Value: value,
 				})
 			}
 
-			resources = append(resources, Resource{
+			resources = append(resources, models.Resource{
 				Provider:   "Kubernetes",
 				Account:    client.Name,
 				Service:    "Pod",
 				ResourceId: string(pod.UID),
 				Name:       pod.Name,
 				Region:     pod.Namespace,
-				Cost:       0,
+				Cost:       podsCost[pod.Name].TotalCost,
 				CreatedAt:  pod.CreationTimestamp.Time,
 				FetchedAt:  time.Now(),
 				Tags:       tags,
