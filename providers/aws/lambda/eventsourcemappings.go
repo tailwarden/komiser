5be13150849acd622dfee778ed13c20cdb11a3d1
Vedank Pande <vedank.pande@gmail.com>
2023-10-05 00:13:47 +0530

    feat: add lambda eventsourcemapping collector

---
+++ providers/aws/lambda/eventsourcemappings.go
@@ -0,0 +1,56 @@
+package lambda
+
+import (
+        "context"
+        "fmt"
+        "strings"
+        "time"
+
+        "github.com/aws/aws-sdk-go-v2/service/lambda"
+        log "github.com/sirupsen/logrus"
+        "github.com/tailwarden/komiser/models"
+        "github.com/tailwarden/komiser/providers"
+)
+
+func EventSourceMappings(ctx context.Context, client providers.ProviderClient) ([]models.Resource, error) {
+        var config lambda.ListEventSourceMappingsInput
+        resources := make([]models.Resource, 0)
+        lambdaClient := lambda.NewFromConfig(*client.AWSClient)
+
+        result, err := lambdaClient.ListEventSourceMappings(context.Background(), &config)
+        if err != nil {
+                log.Errorf("ERROR: Failed to fetch EventSourceMappings: %v", err)
+                return resources, err
+        }
+
+        for _, mapping := range result.EventSourceMappings {
+
+                lambdaNameSplit := strings.Split(*mapping.FunctionArn, ":")
+                lambdaName := lambdaNameSplit[len(lambdaNameSplit)-1]
+
+                resources = append(resources, models.Resource{
+                        Provider:   "AWS",
+                        Account:    client.Name,
+                        Service:    "EventSourceMapping",
+                        ResourceId: *mapping.UUID,
+                        Region:     client.AWSClient.Region,
+                        Name:       *mapping.UUID,
+                        Cost:       0.0,
+                        Metadata: map[string]string{
+                                "lambda": *mapping.EventSourceArn,
+                                "source": *mapping.EventSourceArn,
+                        },
+                        FetchedAt: time.Now(),
+                        Link:      fmt.Sprintf("https://%s.console.aws.amazon.com/lambda/home?region=%s#/functions/%s", client.AWSClient.Region, client.AWSClient.Region, lambdaName),
+                })
+        }
+
+        log.WithFields(log.Fields{
+                "provider":  "AWS",
+                "account":   client.Name,
+                "region":    client.AWSClient.Region,
+                "service":   "EventSourceMapping",
+                "resources": len(resources),
+        }).Info("Fetched resources")
+        return resources, nil
+}
