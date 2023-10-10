128480c7ab1a4529b2ba7d88fa8038e360839ac3
Azanul <azanulhaque@gmail.com>
2023-10-10 20:57:52 +0530

    add OVH alerts
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ providers/ovh/ovh.go
@@ -0,0 +1,41 @@
+package scaleway
+
+import (
+	"context"
+	"log"
+
+	"github.com/sirupsen/logrus"
+	"github.com/tailwarden/komiser/providers/ovh/alerting"
+	"github.com/tailwarden/komiser/utils"
+
+	"github.com/tailwarden/komiser/providers"
+	"github.com/uptrace/bun"
+)
+
+func listOfSupportedServices() []providers.FetchDataFunction {
+	return []providers.FetchDataFunction{
+		alerting.Alerts,
+	}
+}
+
+func FetchResources(ctx context.Context, client providers.ProviderClient, db *bun.DB, telemetry bool, analytics utils.Analytics) {
+	for _, fetchResources := range listOfSupportedServices() {
+		resources, err := fetchResources(ctx, client)
+		if err != nil {
+			log.Printf("[%s][Scaleway] %s", client.Name, err)
+		} else {
+			for _, resource := range resources {
+				_, err := db.NewInsert().Model(&resource).On("CONFLICT (resource_id) DO UPDATE").Set("cost = EXCLUDED.cost").Exec(context.Background())
+				if err != nil {
+					logrus.WithError(err).Errorf("db trigger failed")
+				}
+			}
+			if telemetry {
+				analytics.TrackEvent("discovered_resources", map[string]interface{}{
+					"provider":  "Scaleway",
+					"resources": len(resources),
+				})
+			}
+		}
+	}
+}
