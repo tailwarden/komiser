b850f2a12f55b9733dc936766ed298ca195c5fad
Azanul <azanulhaque@gmail.com>
2023-10-27 22:47:03 +0530

    feat: ovh k8s clusters & nodes
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ providers/ovh/kube/node.go
@@ -0,0 +1,66 @@
+package kube
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/tailwarden/komiser/models"
+	"github.com/tailwarden/komiser/providers"
+	"github.com/tailwarden/komiser/providers/ovh/utils"
+)
+
+type node struct {
+	Properties struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"properties"`
+}
+
+func Nodes(_ context.Context, client providers.ProviderClient) ([]models.Resource, error) {
+	resources := []models.Resource{}
+
+	projectIds, err := utils.GetProjects(client)
+	if err != nil {
+		return resources, err
+	}
+
+	for _, projectId := range projectIds {
+		kubeIds := []string{}
+		err = client.OVHClient.Get(fmt.Sprintf("/v2/cloud/project/%s/kube", projectId), &kubeIds)
+		if err != nil {
+			return resources, err
+		}
+
+		for _, kubeId := range kubeIds {
+			nodes := []node{}
+			err = client.OVHClient.Get(fmt.Sprintf("/v2/cloud/project/%s/kube/%s/node", projectId, kubeId), &nodes)
+			if err != nil {
+				return resources, err
+			}
+
+			for _, node := range nodes {
+				resources = append(resources, models.Resource{
+					Provider:   "OVH",
+					Account:    client.Name,
+					Service:    "Instance",
+					Region:     client.OVHClient.Endpoint(),
+					ResourceId: node.Properties.Id,
+					Cost:       0,
+					Name:       node.Properties.Name,
+					FetchedAt:  time.Now(),
+				})
+			}
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"provider":  "OVH",
+		"account":   client.Name,
+		"service":   "Instance",
+		"resources": len(resources),
+	}).Info("Fetched resources")
+	return resources, nil
+}
