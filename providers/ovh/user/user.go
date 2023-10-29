0a358a2626783ef7658f0ee0065fb55fddac71d3
Azanul <azanulhaque@gmail.com>
2023-10-29 10:55:39 +0530

    feat: ovh storage & user
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ providers/ovh/user/user.go
@@ -0,0 +1,58 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/tailwarden/komiser/models"
+	"github.com/tailwarden/komiser/providers"
+	"github.com/tailwarden/komiser/providers/ovh/utils"
+)
+
+type user struct {
+	Properties struct {
+		Id       string `json:"id"`
+		Username string `json:"username"`
+	} `json:"properties"`
+}
+
+func Instances(_ context.Context, client providers.ProviderClient) ([]models.Resource, error) {
+	resources := []models.Resource{}
+
+	projectIds, err := utils.GetProjects(client)
+	if err != nil {
+		return resources, err
+	}
+
+	for _, projectId := range projectIds {
+		users := []user{}
+		err = client.OVHClient.Get(fmt.Sprintf("/v2/cloud/project/%s/user", projectId), &users)
+		if err != nil {
+			return resources, err
+		}
+
+		for _, user := range users {
+			resources = append(resources, models.Resource{
+				Provider:   "OVH",
+				Account:    client.Name,
+				Service:    "User",
+				Region:     client.OVHClient.Endpoint(),
+				ResourceId: user.Properties.Id,
+				Cost:       0,
+				Name:       user.Properties.Username,
+				FetchedAt:  time.Now(),
+			})
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"provider":  "OVH",
+		"account":   client.Name,
+		"service":   "User",
+		"resources": len(resources),
+	}).Info("Fetched resources")
+	return resources, nil
+}
