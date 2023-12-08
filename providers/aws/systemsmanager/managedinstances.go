59189f785aa63b04f595a7b3b549b409a1a8e87c
horiodino <holiodin@gmail.com>
2023-12-08 23:25:31 +0530

    Reduced API Requests & bulk data fetching for instances.

--- providers/aws/systemsmanager/managedinstances.go
+++ providers/aws/systemsmanager/managedinstances.go
@@ -15,111 +15,77 @@ import (
 	"github.com/tailwarden/komiser/providers"
 )
 
-func getMangedEc2(ctx context.Context, client providers.ProviderClient) ([]models.Resource, error) {
+func getManagedEc2(ctx context.Context, client providers.ProviderClient) ([]models.Resource, error) {
 	resources := make([]models.Resource, 0)
 
 	ssmClient := ssm.NewFromConfig(*client.AWSClient)
+	ec2Client := ec2.NewFromConfig(*client.AWSClient)
 
-	output, err := ssmClient.DescribeInstanceInformation(ctx, &ssm.DescribeInstanceInformationInput{
+	ssmOutput, err := ssmClient.DescribeInstanceInformation(ctx, &ssm.DescribeInstanceInformationInput{
 		MaxResults: aws.Int32(50),
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, ec2instance := range output.InstanceInformationList {
-		running, err := isRunning(ctx, *ec2instance.InstanceId, client)
-		if err != nil {
-			log.Fatal(err)
-		}
+	instanceIds := make([]string, 0, len(ssmOutput.InstanceInformationList))
+	for _, ec2instance := range ssmOutput.InstanceInformationList {
+		instanceIds = append(instanceIds, *ec2instance.InstanceId)
+	}
+	ec2Output, err := ec2Client.DescribeInstances(ctx, &ec2.DescribeInstancesInput{
+		InstanceIds: instanceIds,
+	})
+	if err != nil {
+		return nil, err
+	}
 
-		if true == running {
-			fmt.Println("running")
-			instance, err := getInstanceType(ctx, *ec2instance.InstanceId, client)
-			if err != nil {
-				return nil, err
-			}
+	account, accountID, err := fetchID(ctx, client)
+	if err != nil {
+		return nil, err
+	}
 
-			if len(instance.Reservations) > 0 && len(instance.Reservations[0].Instances) > 0 {
-				currentInstance := instance.Reservations[0].Instances[0]
+	for _, ec2instance := range ec2Output.Reservations {
+		for _, instance := range ec2instance.Instances {
 
+			if instance.State.Name == types.InstanceStateNameRunning {
 				tags := make([]models.Tag, 0)
-				for _, tag := range currentInstance.Tags {
+				for _, tag := range instance.Tags {
 					tags = append(tags, models.Tag{
 						Key:   *tag.Key,
 						Value: *tag.Value,
 					})
 				}
-				account, account_id := fetchid()
 
 				resources = append(resources, models.Resource{
 					Provider:   "AWS",
 					Account:    account,
-					AccountId:  account_id,
+					AccountId:  accountID,
 					Service:    "EC2",
 					Region:     client.AWSClient.Region,
-					ResourceId: *currentInstance.InstanceId,
-					Name:       string(currentInstance.InstanceType),
-					CreatedAt:  *currentInstance.LaunchTime,
+					ResourceId: *instance.InstanceId,
+					Name:       string(instance.InstanceType),
+					CreatedAt:  *instance.LaunchTime,
 					FetchedAt:  time.Now(),
 					Tags:       tags,
 					Link: fmt.Sprintf("https://%s.console.aws.amazon.com/ec2/home?region=%s#InstanceDetails:instanceId=%s",
-						client.AWSClient.Region, client.AWSClient.Region, *ec2instance.InstanceId),
+						client.AWSClient.Region, client.AWSClient.Region, *instance.InstanceId),
 				})
 			}
+
 		}
 	}
 
 	return resources, nil
 }
 
-func getInstanceType(ctx context.Context, instanceID string, client providers.ProviderClient) (*ec2.DescribeInstancesOutput, error) {
-	config := ec2.DescribeInstancesInput{
-		InstanceIds: []string{instanceID},
-	}
-	ec2Client := ec2.NewFromConfig(*client.AWSClient)
-
-	output, err := ec2Client.DescribeInstances(ctx, &config)
-	if err != nil {
-		return output, err
-	}
-
-	return output, nil
-}
-
-func isRunning(ctx context.Context, instanceID string, client providers.ProviderClient) (running bool, err error) {
-	fmt.Println("status check for instance id: ", instanceID)
-	config := ec2.DescribeInstancesInput{
-		InstanceIds: []string{instanceID},
-	}
-	ec2Client := ec2.NewFromConfig(*client.AWSClient)
-
-	output, err := ec2Client.DescribeInstances(ctx, &config)
-	if err != nil {
-		return false, err
-	}
-
-	for _, reservations := range output.Reservations {
-		for _, instance := range reservations.Instances {
-			if instance.State.Name == types.InstanceStateNameRunning {
-				return true, nil
-			}
-		}
-	}
-	return false, nil
-}
-
-func fetchid() (accountID string, userid string) {
-	svc := sts.NewFromConfig(*newclient())
-	result, err := svc.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{})
+func fetchID(ctx context.Context, client providers.ProviderClient) (accountID string, userID string, err error) {
+	svc := sts.NewFromConfig(*client.AWSClient)
+	result, err := svc.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
 	if err != nil {
 		fmt.Println("Got error retrieving account ID:")
-		log.Println(err.Error())
+		return "", "", err
 	}
 
-	// if the accountID and userid same then the account is root account
-	accountID = *result.Account
-	userid = *result.UserId
-
-	return accountID, userid
+	// if the accountID and userID are the same, then the account is a root account
+	return *result.Account, *result.UserId, nil
 }
