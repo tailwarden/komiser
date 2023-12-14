86d062de533bd9cef92bd305cd30924421d70333
Azanul <azanulhaque@gmail.com>
2023-12-14 12:27:38 +0530

    feat: cache costexplorer output last 6 months ending last month
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- utils/datecalc.go
+++ utils/datecalc.go
@@ -25,3 +25,11 @@ func BeginningMonthsAgo(date time.Time, months int) time.Time {
 	}
 	return beginningOfTargetMonth
 }
+
+func EndingOfLastMonth(date time.Time) time.Time {
+	endingOfMonth, err := time.Parse("2006-01-02", date.AddDate(0, 0, -date.Day()+1).Format("2006-01-02"))
+	if err != nil {
+		return time.Now()
+	}
+	return endingOfMonth
+}
