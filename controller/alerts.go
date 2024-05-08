72e8aa13902661aa37e3b3f2f48a1dec352464f1
Azanul <azanulhaque@gmail.com>
2024-05-08 12:21:06 +0530

    feat: alerts controller
    Signed-off-by: Azanul <azanulhaque@gmail.com>

---
+++ controller/alerts.go
@@ -0,0 +1,26 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/tailwarden/komiser/models"
+	"github.com/tailwarden/komiser/repository"
+)
+
+func (ctrl *Controller) InsertAlert(c context.Context, alert models.Alert) (alertId int64, err error) {
+	result, err := ctrl.repo.HandleQuery(c, repository.InsertKey, &alert, nil)
+	if err != nil {
+		return
+	}
+	return result.LastInsertId()
+}
+
+func (ctrl *Controller) UpdateAlert(c context.Context, alert models.Alert, alertId string) (err error) {
+	_, err = ctrl.repo.HandleQuery(c, repository.UpdateAlertKey, &alert, [][3]string{{"id", "=", alertId}})
+	return
+}
+
+func (ctrl *Controller) DeleteAlert(c context.Context, alertId string) (err error) {
+	_, err = ctrl.repo.HandleQuery(c, repository.DeleteKey, new(models.Alert), [][3]string{{"id", "=", alertId}})
+	return
+}
