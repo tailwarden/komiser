724ebb6a86eeae9ece14851a6f79b482f8114207
Azanul <azanulhaque@gmail.com>
2024-05-09 10:06:53 +0530

    feat: stats controller
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- controller/controller.go
+++ controller/controller.go
@@ -15,6 +15,22 @@ type costOutput struct {
 	Total float64 `bun:"sum" json:"total"`
 }
 
+type regionOutput struct {
+	Region string `bun:"region" json:"region"`
+}
+
+type providerOutput struct {
+	Provider string `bun:"provider" json:"provider"`
+}
+
+type serviceOutput struct {
+	Service string `bun:"service" json:"service"`
+}
+
+type accountOutput struct {
+	Account string `bun:"account" json:"account"`
+}
+
 type Repository interface {
 	HandleQuery(context.Context, repository.QueryType, interface{}, [][3]string) (sql.Result, error)
 }
